4edf2e0346b180e9fa811088de5d39aac806557e
Стоян Стоянов <sstoyanov87@mail.bg>
2016-04-09 02:07:38 +0300

    Did some exercise on Data types and variables

---
+++ DataTypesAndVariables/Ex05BooleanVariable/BooleanVariable.cs
@@ -0,0 +1,17 @@
+﻿namespace Ex05BooleanVariable
+{
+    // 05. Declare a Boolean variable called isFemale and assign an appropriate value corresponding to your gender. 
+    // Print it on the console.
+
+    using System;
+
+    public class BooleanVariable
+    {
+        public static void Main()
+        {
+            bool isFemale = false;
+
+            Console.WriteLine("Gender: {0}", isFemale ? "Female" : "Male");
+        }
+    }
+}
---
+++ DataTypesAndVariables/Ex07QuotesInStrings/QuotesInStrings.cs
@@ -0,0 +1,16 @@
+﻿namespace Ex07QuotesInStrings
+{
+    // 07. Write a program that outputs The "use" of quotations causes difficulties. 
+
+    using System;
+
+    public class QuotesInStrings
+    {
+        public static void Main()
+        {
+            var quote = "The \"use\" of quotations causes difficulties.";
+
+            Console.WriteLine(quote);
+        }
+    }
+}
