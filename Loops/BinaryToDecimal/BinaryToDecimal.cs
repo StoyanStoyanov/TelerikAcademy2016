e159e56259916f2090c763a42a07d4a4ecbb67dd
Стоян Стоянов <sstoyanov87@mail.bg>
2016-04-17 01:34:39 +0300

    Solved a few more tasks on Loops

---
+++ Loops/BinaryToDecimal/BinaryToDecimal.cs
@@ -0,0 +1,63 @@
+﻿namespace BinaryToDecimal
+{
+    /*
+        11. Binary to Decimal
+
+        Description
+
+        Using loops write a program that converts a binary integer number to its decimal form.
+
+        The input is entered as string. The output should be a variable of type long.
+        Do not use the built-in .NET functionality.
+        Input
+
+        You will receive exactly one line containing an integer number representation in binary
+        Output
+
+        On the only output line write the decimal representation of the number
+        Constraints
+
+        All input numbers will be valid 32-bit integers
+        Time limit: 0.1s
+        Memory limit: 16MB
+    */
+
+    using System;
+
+    public class BinaryToDecimal
+    {
+        public static void Main()
+        {
+            string binary = Console.ReadLine();
+            Console.WriteLine(ConvertToDecimal(binary, 2));
+        }
+
+        private static long ConvertToDecimal(string representation, int sourceBase)
+        {
+            long numberInDec = 0;
+
+            for (int i = 0, repLen = representation.Length; i < repLen; i++)
+            {
+                var currentBit = representation[repLen - i - 1];
+                if (currentBit == '1')
+                {
+                    numberInDec += Raise(sourceBase, i);
+                }
+            }
+
+            return numberInDec;
+        }
+
+        private static long Raise(int number, int power)
+        {
+            long raised = 1;
+
+            for (int i = 0; i < power; i++)
+            {
+                raised *= number;
+            }
+
+            return raised;
+        }
+    }
+}
---
+++ Loops/HexToDecimal/HexToDecimal.cs
@@ -0,0 +1,74 @@
+﻿namespace HexToDecimal
+{
+    /*
+        14. Hex to Decimal
+
+        Description
+
+        Using loops write a program that converts a hexadecimal integer number to its decimal form.
+
+        The input is entered as string. The output should be a variable of type long.
+        Do not use the built-in .NET functionality.
+        Input
+
+        The input will consists of a single line containing a single hexadecimal number as string.
+        Output
+
+        The output should consist of a single line - the decimal representation of the number as string.
+        Constraints
+
+        All numbers will be valid 64-bit integers.
+        Time limit: 0.1s
+        Memory limit: 16MB
+    */
+
+    using System;
+    using System.Text;
+
+    public class HexToDecimal
+    {
+        public static void Main()
+        {
+            string hex = Console.ReadLine();
+
+            long number = ConvertToDecimal(hex);
+
+            Console.WriteLine(number);
+        }
+
+        private static long ConvertToDecimal(string hex)
+        {
+            long number = 0;
+
+            for (int i = hex.Length - 1, p = 0; i >= 0; i--, p++)
+            {
+                switch (hex[i])
+                {
+                    case 'A': number += 10 * Raise(16, p); break;
+                    case 'B': number += 11 * Raise(16, p); break;
+                    case 'C': number += 12 * Raise(16, p); break;
+                    case 'D': number += 13 * Raise(16, p); break;
+                    case 'E': number += 14 * Raise(16, p); break;
+                    case 'F': number += 15 * Raise(16, p); break;
+                    default:
+                        number += byte.Parse(hex[i].ToString()) * Raise(16, p);
+                        break;
+                }
+            }
+
+            return number;
+        }
+
+        public static long Raise(long number, int power)
+        {
+            long raised = 1;
+
+            for (int i = 0; i < power; i++)
+            {
+                raised *= number;
+            }
+
+            return raised;
+        }
+    }
+}
