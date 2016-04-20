1ddf7409dffce71be55b30b3054cb874c6388895
Стоян Стоянов <sstoyanov87@mail.bg>
2016-04-21 00:19:36 +0300

    Solved  a few more tasks

---
+++ ExamPreparation/Task01Money/Task01Money.cs
@@ -0,0 +1,14 @@
+﻿namespace Task01Money
+{
+    // http://bgcoder.com/Contests/Practice/Index/203#0
+
+    using System;
+
+    public class Task01Money
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ ExamPreparation/Task02EncodingSum/Task02EncodingSum.cs
@@ -0,0 +1,39 @@
+﻿namespace Task02EncodingSum
+{
+    // http://bgcoder.com/Contests/Practice/Index/203#1
+
+    using System;
+
+    public class Task02EncodingSum
+    {
+        public static void Main()
+        {
+            var m = int.Parse(Console.ReadLine());
+            var text = Console.ReadLine().ToUpper();
+
+            long result = 0;
+
+            for (int i = 0, len = text.Length; i < len; i++)
+            {
+                char currentChar = text[i];
+                if (currentChar == '@')
+                {
+                    Console.WriteLine(result);
+                    break;
+                }
+                else if (char.IsDigit(currentChar))
+                {
+                    result *= byte.Parse(currentChar.ToString());
+                }
+                else if (char.IsLetter(currentChar))
+                {
+                    result += (currentChar - 65);
+                }
+                else
+                {
+                    result %= m;
+                }
+            }
+        }
+    }
+}
