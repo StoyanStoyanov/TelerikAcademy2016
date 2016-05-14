d7ac63c7c55fcc5bcff93fadf6b7ed050e8ff98a
Стоян Стоянов <sstoyanov87@mail.bg>
2016-05-14 20:50:36 +0300

    Include missing items

---
+++ C#I/03.OperatorsAndExpressions/Ex11ThirdBit/ThirdBit.cs
@@ -0,0 +1,25 @@
+﻿namespace Ex11ThirdBit
+{
+    /* 
+        11. Using bitwise operators, write a program that uses an expression to find the value 
+        of the bit at index 3 of an unsigned integer read from the console.
+        The bits are counted from right to left, starting from bit 0.
+        The result of the expression should be either 1 or 0. Print it on the console.
+    */
+
+    using System;
+
+    public class ThirdBit
+    {
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+            var position = 3;
+
+            var mask = 1 << position;
+            var result = (number & mask) >> position;
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ C#II/02.MultidimensionalArrays/MultidimensionalArrays/Ex03SequenceInMatrix/Ex03SequenceInMatrix.cs
@@ -0,0 +1,94 @@
+﻿namespace Ex03SequenceInMatrix
+{
+    // https://github.com/TelerikAcademy/CSharp-Part-2/tree/master/Topics/02.%20Multidimensional-Arrays/homework/03.%20Sequence%20in%20matrix
+    // 80/100
+    using System;
+
+    public class Ex03SequenceInMatrix
+    {
+        public static void Main()
+        {
+            var nm = Console.ReadLine().Split(' ');
+            var n = int.Parse(nm[0]);
+            var m = int.Parse(nm[1]);
+            var matrix = new string[n, m];
+
+            for (int row = 0; row < n; row++)
+            {
+                var strings = Console.ReadLine().Split(' ');
+                for (int col = 0; col < m; col++)
+                    matrix[row, col] = strings[col];
+            }
+
+            var maxLength = 1;
+
+            for (int row = n - 1; row >= 0; row--)
+            {
+                for (int col = m - 1; col >= 0; col--)
+                {
+                    int bestRowLength = GetBestRowLength(row, col, matrix);
+                    if (maxLength < bestRowLength)
+                        maxLength = bestRowLength;
+
+                    int bestColLength = GetBestColLength(row, col, matrix);
+                    if (maxLength < bestColLength)
+                        maxLength = bestColLength;
+
+                    int bestDiagLength = GetBestDiagLength(row, col, matrix);
+                    if (maxLength < bestDiagLength)
+                        maxLength = bestDiagLength;
+                }
+            }
+
+            Console.WriteLine(maxLength);
+        }
+
+        private static int GetBestDiagLength(int row, int col, string[,] matrix)
+        {
+            var result = 1;
+            var start = matrix[row, col];
+
+            while (row > 0 && col > 0)
+            {
+                if (matrix[--row, --col] != start)
+                    break;
+
+                result++;
+            }
+
+            return result;
+        }
+
+        private static int GetBestColLength(int row, int col, string[,] matrix)
+        {
+            var result = 1;
+            var start = matrix[row, col];
+
+            while (row > 0)
+            {
+                if (matrix[--row, col] != start)
+                    break;
+
+                result++;
+            }
+
+            return result;
+        }
+
+        private static int GetBestRowLength(int row, int col, string[,] matrix)
+        {
+            var result = 1;
+            var start = matrix[row, col];
+
+            while (col > 0)
+            {
+                if (matrix[row, --col] != start)
+                    break;
+
+                result++;
+            }
+
+            return result;
+        }
+    }
+}
