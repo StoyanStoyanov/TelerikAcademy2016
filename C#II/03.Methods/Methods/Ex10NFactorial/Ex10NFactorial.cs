bd2d22693c5064a82ba76e79078e818e18c9c50e
Стоян Стоянов <sstoyanov87@mail.bg>
2016-05-15 22:25:28 +0300

    Completed Methods homework

---
+++ C#II/03.Methods/Methods/Ex10NFactorial/Ex10NFactorial.cs
@@ -0,0 +1,31 @@
+﻿namespace Ex10NFactorial
+{
+    // https://github.com/TelerikAcademy/CSharp-Part-2/tree/master/Topics/03.%20Methods/homework/10.%20N%20Factorial
+
+    using System;
+    using System.Numerics;
+
+    public class Ex10NFactorial
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var nFactorial = CalculateFactorial(n);
+
+            Console.WriteLine(nFactorial);
+        }
+
+        private static BigInteger CalculateFactorial(int n)
+        {
+            var factorial = new BigInteger(1);
+
+            for (var i = n; i > 0; i--)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+    }
+}
