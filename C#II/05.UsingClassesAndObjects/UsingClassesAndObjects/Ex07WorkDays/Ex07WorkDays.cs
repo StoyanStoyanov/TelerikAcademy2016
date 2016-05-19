af41c200c623591816a6bb358316645b1e697add
Стоян Стоянов <sstoyanov87@mail.bg>
2016-05-19 23:57:28 +0300

    Solved a few tasks

---
+++ C#II/05.UsingClassesAndObjects/UsingClassesAndObjects/Ex07WorkDays/Ex07WorkDays.cs
@@ -0,0 +1,33 @@
+﻿namespace Ex07WorkDays
+{
+    // https://github.com/TelerikAcademy/CSharp-Part-2/tree/master/Topics/05.%20Using-Classes-and-Objects/homework/07.%20Workdays
+
+    using System;
+
+    public class Ex07WorkDays
+    {
+        public static void Main()
+        {
+            var year = int.Parse(Console.ReadLine());
+            var month = int.Parse(Console.ReadLine());
+            var day = int.Parse(Console.ReadLine());
+
+            var date = new DateTime(year, month, day);
+            var holidays = new DateTime[] { new DateTime(2016, 12, 25), new DateTime(2016, 12, 31) };
+            var workDays = GetWorkDays(DateTime.Now, date, holidays);
+
+            Console.WriteLine(workDays);
+        }
+
+        private static int GetWorkDays(DateTime now, DateTime date, DateTime[] holidays)
+        {
+            var days = date.Subtract(now).Days;
+
+            var weekends = (days / 7) * 2;
+
+            var workDays = days - weekends - holidays.Length;
+
+            return Math.Abs(workDays);
+        }
+    }
+}
