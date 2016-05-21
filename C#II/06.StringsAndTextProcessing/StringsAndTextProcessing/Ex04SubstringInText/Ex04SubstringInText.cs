40626010c154fb482077f89ed2bd2ebfa7534887
Стоян Стоянов <sstoyanov87@mail.bg>
2016-05-22 02:37:59 +0300

    Solved a few tasks

---
+++ C#II/06.StringsAndTextProcessing/StringsAndTextProcessing/Ex04SubstringInText/Ex04SubstringInText.cs
@@ -0,0 +1,47 @@
+﻿namespace Ex04SubstringInText
+{
+    // https://github.com/TelerikAcademy/CSharp-Part-2/tree/master/Topics/06.%20Strings-and-Text-Processing/homework/04.%20Sub-string%20in%20text
+
+    using System;
+    using System.Text;
+
+    public class Ex04SubstringInText
+    {
+        public static void Main()
+        {
+            var pattern = Console.ReadLine();
+            var text = Console.ReadLine();
+
+            var frequency = GetPatternFrequency(pattern, text);
+
+            Console.WriteLine(frequency);
+        }
+
+        private static int GetPatternFrequency(string pattern, string text)
+        {
+            var substring = new StringBuilder();
+            var frequency = 0;
+
+            for (int i = text.Length - 1; i >= pattern.Length - 1; i--)
+            {
+                var match = true;
+
+                for (int j = pattern.Length - 1, k = i; j >= 0; j--, k--)
+                {
+                    if (char.ToLower(pattern[j]) != char.ToLower(text[k]))
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match)
+                {
+                    frequency++;
+                }
+            }
+
+            return frequency;
+        }
+    }
+}
---
+++ C#II/06.StringsAndTextProcessing/StringsAndTextProcessing/Ex06StringLength/Ex06StringLength.cs
@@ -0,0 +1,12 @@
+﻿namespace Ex06StringLength
+{
+    using System;
+
+    public class Ex06StringLength
+    {
+        public static void Main()
+        {
+            Console.Write(Console.ReadLine().Replace("\\", "").PadRight(20, '*'));
+        }
+    }
+}
