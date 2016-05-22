6ec02bbccaec3cdaeae9daa27bc0933c2779b87a
Стоян Стоянов <sstoyanov87@mail.bg>
2016-05-22 16:26:04 +0300

    small fixes

--- C#II/06.StringsAndTextProcessing/StringsAndTextProcessing/Ex08ExtractSentences/Ex08ExtractSentences.cs
+++ C#II/06.StringsAndTextProcessing/StringsAndTextProcessing/Ex08ExtractSentences/Ex08ExtractSentences.cs
@@ -7,34 +7,34 @@
     {
         public static void Main()
         {
-            var word = Console.ReadLine().ToUpperInvariant();
-            var txt = "." + Console.ReadLine() + ".";
+            var word = Console.ReadLine().ToUpper();
+            var text = "." + Console.ReadLine();
             var sb = new StringBuilder();
 
-            for (int i = 0, j = 0; i < txt.Length; i++)
+            for (int i = 0, j = 0; i < text.Length; i++)
             {
                 var match = true;
 
                 for (j = i; j < i + word.Length; j++)
                 {
-                    if (char.ToUpperInvariant(txt[j]) != word[j - i])
+                    if (char.ToUpper(text[j]) != word[j - i])
                     {
                         match = false;
                         break;
                     }
                 }
-
-                if (match && !char.IsLetter(txt[i - 1]) && !char.IsLetter(txt[j]))
-                {
-                    while (txt[--i] != '.');
-                    while (txt[++i] != '.')
-                        sb.Append(txt[i]);
-
+                
+                if (match && !char.IsLetter(text[i - 1]) 
+                          && !char.IsLetter(text[j]))
+                {   // go back to sentence start
+                    while (text[--i] != '.'); 
+                    while (text[++i] != '.') 
+                        sb.Append(text[i]);
                     sb.Append('.');
                 }
             }
 
-            Console.WriteLine(sb);
+            Console.Write(sb);
         }
     }
 }
