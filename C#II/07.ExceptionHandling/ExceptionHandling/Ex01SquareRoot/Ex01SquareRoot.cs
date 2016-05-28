6826dfedf9e5809be0b82f644e977c87896cbefb
Стоян Стоянов <sstoyanov87@mail.bg>
2016-05-28 22:03:56 +0300

    added Exception Handling

---
+++ C#II/07.ExceptionHandling/ExceptionHandling/Ex01SquareRoot/Ex01SquareRoot.cs
@@ -0,0 +1,45 @@
+﻿namespace Ex01SquareRoot
+{
+    // https://github.com/TelerikAcademy/CSharp-Part-2/tree/master/Topics/07.%20Exception-Handling/homework/01.%20Square%20root
+
+    using System;
+
+    public class Ex01SquareRoot
+    {
+        public static void Main()
+        {
+            try
+            {
+                var number = double.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    Console.WriteLine("Invalid number");
+                }
+                else
+                {
+                    Console.WriteLine("{0:F3}", Math.Sqrt(number));
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch (NotFiniteNumberException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
